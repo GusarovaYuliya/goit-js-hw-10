38817c8595092598d79b63b90897990b1726e31f
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-09-09 22:06:07 +0300

    comm

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import { TheCatAPI } from "@thatapicompany/thecatapi";
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_s71qm7HgSYUOPT7htQYja4LKWwIQBrnddCpXxJ8m6HQ27I6dW3ItLskFAbIx7x1D";
+
+import SlimSelect from 'slim-select'
+
+new SlimSelect({
+  select: '#selectElement'
+})
+
+import Notiflix from 'notiflix';
